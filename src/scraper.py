c0e17a2ea839249752d15e672f16e579a6c98fa9
pescap <pescapil@uc.cl>
2023-04-06 10:13:22 -0400

    added code

---
+++ src/scraper.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+import datetime
+
+# fecha y nombre del archivo
+
+
+
+more = True
+
+name = 'output/data.csv'
+
+more_pages = True
+page = 1
+
+
+while more_pages == True:
+	print(page)
+	if page == 1:
+		url_name = "https://www.carabineros.cl/detalleMemorial.php"
+	else:
+		url_name = "https://www.carabineros.cl/detalleMartir.php?pagina=" + str(page)
+
+	url = requests.get(url_name)
+
+
+	soup = BeautifulSoup(url.text, "html.parser")
+
+
+	profiles = soup.find_all("div", attrs={"class": "card-content-index"})
+		       
+	columns = ['date', 'grade', 'name', 'station']
+	df = pd.DataFrame(columns=columns)
+
+
+	for profile in profiles:
+		grade = profile.findChildren('h2')[0].get_text()
+		name = profile.findChildren('h1')[0].get_text()
+		date = profile.findChildren('strong')[0].get_text()
+		station= profile.findChildren('strong')[1].get_text()
+		
+		date = date.replace('-','').replace(' ', '')
+		day = int(date[:2])
+		month = int(date[2:4])
+		year = int(date[4:8])
+		
+		date = datetime.datetime(year, month, day)
+		print(grade, name, date, station)
+		new_row = {
+		"date": date,
+		"grade": grade,
+		"name": name,
+		"station": station}
+		
+		df = pd.concat([df, pd.DataFrame([new_row])], axis=0, ignore_index=True)
+	
+
+	if profiles == []:
+		more_pages = False
+	page += 1
+
+
+df.to_csv(name, index=False)
